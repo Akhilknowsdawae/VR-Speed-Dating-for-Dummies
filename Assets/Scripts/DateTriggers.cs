467701858b5ee3ca3decce4f2f244f481fc5631e
Ray <rayrmrose@gmail.com>
2026-04-01 01:25:47 -0400

    Added crossfade

--- Assets/Scripts/DateTriggers.cs
+++ Assets/Scripts/DateTriggers.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class DateTriggers : MonoBehaviour
 {
     public GameObject teleportSpots;
-    public GameObject dateUIa;
-    public GameObject dateUIb;
+    public GameObject dateUI_A;
+    public GameObject dateUI_B;
+
+    public Animator transition;
+    public float transitionTime = 1f;
     // Start is called before the first frame update
     void Start()
     {
         teleportSpots.SetActive(false);
-        dateUIb.SetActive(false);
+        dateUI_B.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,13 +23,36 @@ public class DateTriggers : MonoBehaviour
         
     }
 
+    public void GoToFirstDate()
+    {
+        Debug.Log("Going to second date!");
+        Vector3 newPos = new Vector3(-1.45299995f, -6.69899988f, 3.78800011f);
+        Vector3 oldPos = new Vector3(3f, -6.69899988f, 3.78800011f);
+
+        StartCoroutine( CrossfadeTransition(newPos) );
+
+        
+        dateUI_A.SetActive(true);
+        dateUI_B.SetActive(false);
+    }
     public void GoToSecondDate()
     {
         Debug.Log("Going to second date!");
         Vector3 oldPos = new Vector3(-1.45299995f, -6.69899988f, 3.78800011f);
         Vector3 newPos = new Vector3(3f, -6.69899988f, 3.78800011f);
+
+        StartCoroutine( CrossfadeTransition(newPos) );
+
+        
+        dateUI_A.SetActive(false);
+        dateUI_B.SetActive(true);
+    }
+
+    IEnumerator CrossfadeTransition(Vector3 newPos)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
         transform.position = newPos;
-        dateUIa.SetActive(false);
-        dateUIb.SetActive(true);
     }
 }
